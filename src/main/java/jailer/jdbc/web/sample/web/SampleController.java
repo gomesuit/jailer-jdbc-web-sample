573b86a70c3f89805a6e1e5f8de7d963c37d4c10
gomesuit <gomesuit@gmail.com>
2016-01-15 00:54:44 +0900

    commit

--- src/main/java/jailer/jdbc/web/sample/web/SampleController.java
+++ src/main/java/jailer/jdbc/web/sample/web/SampleController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ManagerTopController {
+public class SampleController {
 	@Autowired
 	private SampleMapper sampleMapper;
     
